fdecbe63513827c7203d18e0d65c4fe5de6e46ab
Jungmin Cha <ckwjdals90@gmail.com>
2019-12-31 12:30:30 -0800

    ueberauth deps installed and github key added

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,10 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [default_scope: "user,user:email,public_repo"] }
+  ]
+
+import_config "config.secret.exs"
